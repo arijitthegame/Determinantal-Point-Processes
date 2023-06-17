374f94aaf879b28d6ebf9644e9db735c28a8b4f9
Arijit Sehanobish <arijitthegame@gmail.com>
2023-06-17 18:19:20 -0400

    add mcmc utils

--- src/utils.py
+++ src/utils.py
@@ -39,3 +39,31 @@ def get_nonuniform_cluster_matrix(n, m, k):
             (points_per_cluster,))
 
     return B[np.random.permutation(n)]
+
+def psd_matrix_sqrt(A):
+    eig_vals, eig_vecs = np.linalg.eigh(A)
+    idx = eig_vals > 1e-15
+    return eig_vecs[:,idx] * np.sqrt(eig_vals[idx])
+
+
+
+def elementary_polynomial(k, eigen_vals):
+    n = len(eigen_vals)
+    E_poly = np.zeros((k + 1, n + 1)).astype(eigen_vals.dtype)
+    E_poly[0, :] = 1
+    for l in range(1, k + 1):
+        for n in range(1, n + 1):
+            E_poly[l, n] = E_poly[l, n - 1] + eigen_vals[n - 1] * E_poly[l - 1, n - 1]
+    return E_poly
+
+
+def sample_kdpp_eigen_vecs(k, eig_vals, E_poly):
+    ind_selected = np.zeros(k, dtype=int)
+    for n in range(len(eig_vals), 0, -1):
+        rand_nums = np.random.rand()
+        if rand_nums < eig_vals[n - 1] * E_poly[k - 1, n - 1] / E_poly[k, n]:
+            k -= 1
+            ind_selected[k] = n - 1
+            if k == 0:
+                break
+    return ind_selected
